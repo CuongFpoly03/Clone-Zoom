69557bc7b44bc8a61e0339bc9b0da699e2cc84a5
cuonglp <cuongfpoly813@gmail.com>
2024-05-21 10:05:48 +0700

    first commit

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,13 @@
+import CallList from '@/components/home/CallList';
+
+const PreviousPage = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 ">
+      <h1 className="text-3xl font-bold">Recordings</h1>
+
+      <CallList type="recordings" />
+    </section>
+  );
+};
+
+export default PreviousPage;
